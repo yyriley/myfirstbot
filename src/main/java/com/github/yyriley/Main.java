7c68fd4a1093adf900f1a3bc67493248f73bc9fc
Yrua Riley <yyriley0124@gmail.com>
2021-10-03 05:27:04 -0500

    Initial commit

---
+++ src/main/java/com/github/yyriley/Main.java
@@ -0,0 +1,25 @@
+package com.github.yyriley;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Insert your bot's token here
+        String token = "ODkzNTcwODIwNzg4NjA5MDc1.YVdYrQ.pdfwJs9mC1VNH4Efjx_hMxQyQX0";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
